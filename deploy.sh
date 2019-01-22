475ff19e24ba2a7cb835f69f8fc47c54ef865ce9
smalin <dinglin0020@gmail.com>
2019-01-22 18:34:17 +0800

    modif deploy file , add permission

--- deploy.sh
+++ deploy.sh
@@ -10,15 +10,15 @@ cd .vuepress/dist
 # 如果是发布到自定义域名
 echo 'smalin.cn' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+sudo git init
+sudo git add -A
+sudo git commit -m 'deploy'
 #git remote add origin https://github.com/Smalin0020/blog.git
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:smalin0020/myBlog.git master
 # git push -f origin master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:smalin0020/myBlog.git master:gh-pages
+sudo git push -f git@github.com:smalin0020/myBlog.git master:gh-pages
 
 cd -
