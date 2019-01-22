f986a99892235821895428006455784c8344e6f3
smalin <dinglin0020@gmail.com>
2019-01-22 18:48:11 +0800

    modif deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,17 @@ vuepress build
 cd .vuepress/dist
 
 # 如果是发布到自定义域名
-sudo echo 'smalin.cn' > CNAME
+echo 'smalin.cn' > CNAME
 
-sudo git init
-sudo git add -A
-sudo git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 #git remote add origin https://github.com/Smalin0020/blog.git
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:smalin0020/myBlog.git master
 # git push -f origin master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-sudo git push -f git@github.com:smalin0020/myBlog.git master:gh-pages
+git push -f git@github.com:smalin0020/myBlog.git master:gh-pages
 
 cd -
